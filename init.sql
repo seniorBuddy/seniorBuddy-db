b5bbea1aa5236b96f16ecd2e6ff87a91e16db11d
Samuel Lee <131708811+samuelitis@users.noreply.github.com>
2024-10-17 07:37:51 +0900

    Update init.sql

--- init.sql
+++ init.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS assistant_threads (
     user_id INT UNIQUE NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     run_state VARCHAR(50) NOT NULL,
-    run_id VARCHAR(100) NOT NULL,
+    run_id VARCHAR(100),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
